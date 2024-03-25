a104a23ef35921566aacfb4b7de66ebc1c7b970c
SoraSuegami <suegamisora@gmail.com>
2024-03-25 22:34:37 +0900

    Update database and web_server

--- packages/relayer/src/database.rs
+++ packages/relayer/src/database.rs
@@ -3,6 +3,28 @@ use crate::*;
 use ic_utils::interfaces::wallet;
 use sqlx::{postgres::PgPool, Row};
 
+#[derive(Debug, Clone)]
+pub struct CodesRow {
+    pub account_code: String,
+    pub wallet_eth_addr: String,
+    pub guardian_email_addr: String,
+    pub is_set: bool,
+}
+
+#[derive(Debug, Clone)]
+pub struct RequestsRow {
+    pub request_id: i64,
+    pub wallet_eth_addr: String,
+    pub guardian_email_addr: String,
+    pub is_for_recovery: bool,
+    pub template_idx: u64,
+    pub is_processed: bool,
+    pub is_success: Option<bool>,
+    pub email_nullifier: Option<String>,
+    pub account_salt: Option<String>,
+    pub is_code_exist: Option<bool>,
+}
+
 pub struct Database {
     db: PgPool,
 }
@@ -105,32 +127,163 @@ impl Database {
     // }
 
     #[named]
-    pub(crate) async fn insert_code(
-        &self,
-        account_code: &str,
-        wallet_eth_addr: &str,
-        guardian_email_addr: &str,
-        is_set: bool,
-    ) -> Result<()> {
+    pub(crate) async fn get_codes_row(&self, account_code: &str) -> Result<Option<CodesRow>> {
+        let row = sqlx::query("SELECT * FROM codes WHERE account_code = $1")
+            .bind(account_code)
+            .fetch_optional(&self.db)
+            .await?;
+
+        match row {
+            Some(row) => {
+                let account_code: String = row.get("account_code");
+                let wallet_eth_addr: String = row.get("wallet_eth_addr");
+                let guardian_email_addr: String = row.get("guardian_email_addr");
+                let is_set: bool = row.get("is_set");
+                let codes_row = CodesRow {
+                    account_code,
+                    wallet_eth_addr,
+                    guardian_email_addr,
+                    is_set,
+                };
+                info!(LOG, "row {:?}", codes_row; "func" => function_name!());
+                Ok(Some(codes_row))
+            }
+            None => Ok(None),
+        }
+    }
+
+    #[named]
+    pub(crate) async fn insert_codes_row(&self, row: &CodesRow) -> Result<()> {
+        info!(LOG, "insert row {:?}", row; "func" => function_name!());
         let row = sqlx::query(
             "INSERT INTO users (account_code, wallet_eth_addr, guardian_email_addr, is_set) VALUES ($1, $2, $3, $4) RETURNING *",
         )
-        .bind(account_code)
-        .bind(wallet_eth_addr)
-        .bind(guardian_email_addr)
-        .bind(is_set)
+        .bind(&row.account_code)
+        .bind(&row.wallet_eth_addr)
+        .bind(&row.guardian_email_addr)
+        .bind(row.is_set)
         .fetch_one(&self.db)
         .await?;
+        info!(
+            LOG,
+            "{} row inserted",
+            row.len(); "func" => function_name!()
+        );
         Ok(())
     }
 
     #[named]
-    pub async fn set_guardian(&self, account_code: &str) -> Result<()> {
+    pub async fn set_guardian_in_codes(&self, account_code: &str) -> Result<()> {
         info!(LOG, "account_code {}", account_code; "func" => function_name!());
         let res = sqlx::query("UPDATE users SET is_set = TRUE WHERE account_code = $1")
             .bind(account_code)
             .execute(&self.db)
             .await?;
+        info!(
+            LOG,
+            "updated result: {:?}",
+            res; "func" => function_name!()
+        );
+        Ok(())
+    }
+
+    #[named]
+    pub(crate) async fn get_requests_row(&self, request_id: i64) -> Result<Option<RequestsRow>> {
+        let row = sqlx::query("SELECT * FROM requests WHERE request_id = $1")
+            .bind(request_id)
+            .fetch_optional(&self.db)
+            .await?;
+
+        match row {
+            Some(row) => {
+                let request_id: i64 = row.get("request_id");
+                let wallet_eth_addr: String = row.get("wallet_eth_addr");
+                let guardian_email_addr: String = row.get("guardian_email_addr");
+                let is_for_recovery: bool = row.get("is_for_recovery");
+                let template_idx: u64 = row.get::<i64, _>("template_idx") as u64;
+                let is_processed: bool = row.get("is_processed");
+                let is_success: Option<bool> = row.get("is_success");
+                let email_nullifier: Option<String> = row.get("email_nullifier");
+                let account_salt: Option<String> = row.get("account_salt");
+                let is_code_exist: Option<bool> = row.get("is_code_exist");
+                let requests_row = RequestsRow {
+                    request_id,
+                    wallet_eth_addr,
+                    guardian_email_addr,
+                    is_for_recovery,
+                    template_idx,
+                    is_processed,
+                    is_success,
+                    email_nullifier,
+                    account_salt,
+                    is_code_exist,
+                };
+                info!(LOG, "row {:?}", requests_row; "func" => function_name!());
+                Ok(Some(requests_row))
+            }
+            None => Ok(None),
+        }
+    }
+
+    #[named]
+    pub(crate) async fn insert_requests_row(&self, row: &RequestsRow) -> Result<()> {
+        info!(LOG, "insert row {:?}", row; "func" => function_name!());
+        let row = sqlx::query(
+            "INSERT INTO requests (request_id, wallet_eth_addr, guardian_email_addr, is_for_recovery, template_idx, is_processed, is_success, email_nullifier, account_salt, is_code_exist) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *",
+        )
+        .bind(&row.request_id)
+        .bind(&row.wallet_eth_addr)
+        .bind(&row.guardian_email_addr)
+        .bind(row.is_for_recovery)
+        .bind(row.template_idx as i64)
+        .bind(row.is_processed)
+        .bind(row.is_success)
+        .bind(&row.email_nullifier)
+        .bind(&row.account_salt)
+        .bind(row.is_code_exist)
+        .fetch_one(&self.db)
+        .await?;
+        info!(
+            LOG,
+            "{} row inserted",
+            row.len(); "func" => function_name!()
+        );
+        Ok(())
+    }
+
+    #[named]
+    pub(crate) async fn request_successed(
+        &self,
+        request_id: i64,
+        email_nullifier: &str,
+        account_salt: &str,
+        is_code_exist: bool,
+    ) -> Result<()> {
+        info!(LOG, "request_id {}", request_id; "func" => function_name!());
+        let res = sqlx::query("UPDATE requests SET is_processed = TRUE, is_success = TRUE, email_nullifier = $1, account_salt = $2, is_code_exist = $3, WHERE request_id = $4")
+            .bind(email_nullifier)
+            .bind(account_salt)
+            .bind(is_code_exist)
+            .bind(request_id)
+            .execute(&self.db)
+            .await?;
+        info!(
+            LOG,
+            "updated result: {:?}",
+            res; "func" => function_name!()
+        );
+        Ok(())
+    }
+
+    #[named]
+    pub(crate) async fn request_failed(&self, request_id: i64) -> Result<()> {
+        info!(LOG, "request_id {}", request_id; "func" => function_name!());
+        let res = sqlx::query(
+            "UPDATE requests SET is_processed = TRUE, is_success = FALSE WHERE request_id = $1",
+        )
+        .bind(request_id)
+        .execute(&self.db)
+        .await?;
         info!(
             LOG,
             "updated result: {:?}",
