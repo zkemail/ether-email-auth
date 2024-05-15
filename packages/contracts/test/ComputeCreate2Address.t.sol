31b88e17d94de8756c8a77b6c3e21a942f4d07f4
wshino <wshinohara@gmail.com>
2024-05-15 12:47:19 +0900

    Fix testComputeCreate2Address.

--- packages/contracts/test/ComputeCreate2Address.t.sol
+++ packages/contracts/test/ComputeCreate2Address.t.sol
@@ -16,41 +16,21 @@ contract ComputeCreate2AddressTest is StructHelper {
 
     function testComputeCreate2Address() public {
         bytes32 accountSalt = 0x0;
-        // bytes memory bytecode = type(ERC1967Proxy).creationCode;
-        // // address computedAddress;
-        // // assembly {
-        // //     let bytecodeSize := mload(bytecode)
-        // //     computedAddress := create2(0, add(bytecode, 32), bytecodeSize, accountSalt)
-        // //     if iszero(extcodesize(computedAddress)) {
-        // //         revert(0, 0)
-        // //     }
-        // // }
-        // bytes memory creationCode = type(ERC1967Proxy).creationCode;
-        // bytes memory bytecodeHash = abi.encodePacked(
-        //     bytes32(
-        //         0x0000000000000000000000000000000000000000000000000000000000000000
-        //     )
-        // );        
-        // for (uint8 i = 0; i < 32; i++) {
-        //     bytecodeHash[i] = creationCode[4 + 32 + i];
-        // }
-        // address computedAddress = vm.computeCreate2Address(
-        //     accountSalt, 
-        //     keccak256(abi.encodePacked(
-        //                 type(ERC1967Proxy).creationCode, 
-        //                 abi.encode(
-        //                     simpleWallet.emailAuthImplementation(),
-        //                     abi.encodeCall(
-        //                         EmailAuth.initialize,
-        //                         (address(this), accountSalt)
-        //                     )
-        //                 ))), 
-        //     address(this)
-        // );
+
+        string memory artifact = vm.readFile(
+            "zkout/ERC1967Proxy.sol/artifacts.json"
+        );
+        bytes32 bytecodeHash = vm.parseJsonBytes32(
+            artifact,
+            '.contracts.["../../node_modules/@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol"].ERC1967Proxy.hash'
+        );
+        console.log("bytecodeHash");
+        console.logBytes32(bytes32(bytecodeHash));
+
         address computedAddress = L2ContractHelper.computeCreate2Address(
                     address(this),
                     accountSalt,
-                    keccak256(type(ERC1967Proxy).creationCode),
+                    bytes32(bytecodeHash),
                     keccak256(
                         abi.encode(
                             simpleWallet.emailAuthImplementation(),
@@ -61,21 +41,6 @@ contract ComputeCreate2AddressTest is StructHelper {
                         )
                     )
         );
-        // bytes32 senderBytes = bytes32(uint256(uint160(address(this))));
-        // bytes32 bytecodeHash = keccak256(type(ERC1967Proxy).creationCode);
-        // bytes32 constructorInputHash = keccak256(
-        //     abi.encode(
-        //         simpleWallet.emailAuthImplementation(),
-        //         abi.encodeCall(
-        //             EmailAuth.initialize,
-        //             (address(this), accountSalt)
-        //         )
-        //     )
-        // );
-        // bytes32 data = keccak256(
-        //     bytes.concat(senderBytes, accountSalt, bytecodeHash, constructorInputHash)
-        // );
-        // computedAddress = address(uint160(uint256(data)));
 
         console.log("computedAddress", computedAddress);
 
