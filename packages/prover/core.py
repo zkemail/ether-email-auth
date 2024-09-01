92be537438ef900449aa561a02e8d4309c128c31
SoraSuegami <suegamisora@gmail.com>
2024-09-01 15:07:38 +0900

    Add python logger

--- packages/prover/core.py
+++ packages/prover/core.py
@@ -1,18 +1,21 @@
 import subprocess
 import os
 import json
+import logging
 
 
-def gen_email_auth_proof(nonce: str, is_local: bool, input: dict) -> dict:
+def gen_email_auth_proof(
+    nonce: str, is_local: bool, input: dict, logger: logging.Logger
+) -> dict:
     circuit_name = "email_auth"
-    store_input(circuit_name, nonce, input)
-    gen_proof(circuit_name, nonce, is_local)
-    proof = load_proof(circuit_name, nonce)
-    pub_signals = load_pub_signals(circuit_name, nonce)
+    store_input(circuit_name, nonce, input, logger)
+    gen_proof(circuit_name, nonce, is_local, logger)
+    proof = load_proof(circuit_name, nonce, logger)
+    pub_signals = load_pub_signals(circuit_name, nonce, logger)
     return {"proof": proof, "pub_signals": pub_signals}
 
 
-def store_input(circuit_name: str, nonce: str, json_data: dict):
+def store_input(circuit_name: str, nonce: str, json_data: dict, logger: logging.Logger):
     cur_dir = get_cur_dir()
     build_dir = os.path.join(cur_dir, "build")
     # check if build_dir exists
@@ -22,35 +25,40 @@ def store_input(circuit_name: str, nonce: str, json_data: dict):
     json_file_path = os.path.join(
         build_dir, "input_" + circuit_name + "_" + nonce + ".json"
     )
+    logger.trace(f"Store user input {json_data} to {json_file_path}")
     with open(json_file_path, "w") as json_file:
         json_file.write(json_data)
 
 
-def load_proof(circuit_name: str, nonce: str) -> dict:
+def load_proof(circuit_name: str, nonce: str, logger: logging.Logger) -> dict:
     cur_dir = get_cur_dir()
     build_dir = os.path.join(cur_dir, "build")
     json_file_path = os.path.join(
         build_dir, "rapidsnark_proof_" + circuit_name + "_" + nonce + ".json"
     )
+    logger.trace(f"Loading proof from {json_file_path}")
     with open(json_file_path, "r") as json_file:
         return json.loads(json_file.read())
 
 
-def load_pub_signals(circuit_name: str, nonce: str) -> dict:
+def load_pub_signals(circuit_name: str, nonce: str, logger: logging.Logger) -> dict:
     cur_dir = get_cur_dir()
     build_dir = os.path.join(cur_dir, "build")
     json_file_path = os.path.join(
         build_dir, "rapidsnark_public_" + circuit_name + "_" + nonce + ".json"
     )
+    logger.trace(f"Loading public signals from {json_file_path}")
     with open(json_file_path, "r") as json_file:
         return json.loads(json_file.read())
 
 
-def gen_proof(circuit_name: str, nonce: str, is_local: bool):
+def gen_proof(circuit_name: str, nonce: str, is_local: bool, logger: logging.Logger):
     is_local_int: int = 1 if is_local else 0
     cur_dir = get_cur_dir()
     params_dir = os.path.join(cur_dir, "params")
+    logger.trace(f"Params dir: {params_dir}")
     build_dir = os.path.join(cur_dir, "build")
+    logger.trace(f"Build dir: {build_dir}")
     result = subprocess.run(
         [
             os.path.join(cur_dir, "circom_proofgen.sh"),
@@ -61,6 +69,9 @@ def gen_proof(circuit_name: str, nonce: str, is_local: bool):
             str(is_local_int),
         ]
     )
+    logger.info(f"Proof generation result: {result.returncode}")
+    if result.stderr is not None:
+        logger.error(result.stderr)
     print(result.stdout)
     print(result.stderr)
 
--- packages/prover/modal_server.py
+++ packages/prover/modal_server.py
@@ -1,41 +1,57 @@
 import modal
 
-from core import (
-    gen_email_auth_proof,
-)
 
-
-stub = modal.Stub("email-auth-prover-v1.0.3")
+app = modal.App("email-auth-prover-v1.0.4")
 
 image = modal.Image.from_dockerfile("Dockerfile")
 
 
-@stub.function(
+@app.function(
     image=image,
     mounts=[
         modal.Mount.from_local_python_packages("core"),
     ],
     cpu=14,
+    secrets=[modal.Secret.from_name("gc-ether-email-auth-prover")],
 )
 @modal.wsgi_app()
 def flask_app():
     from flask import Flask, request, jsonify
     import random
     import sys
+    import json
+    import os
+    import logging
+    from google.cloud.logging import Client
+    from google.cloud.logging.handlers import CloudLoggingHandler
+    from google.oauth2 import service_account
+    from core import (
+        gen_email_auth_proof,
+    )
 
     app = Flask(__name__)
+    service_account_info = json.loads(os.environ["SERVICE_ACCOUNT_JSON"])
+    credentials = service_account.Credentials.from_service_account_info(
+        service_account_info
+    )
+    logging_client = Client(project="zkairdrop", credentials=credentials)
+    handler = CloudLoggingHandler(logging_client, name="ether-email-auth-prover")
 
     @app.post("/prove/email_auth")
     def prove_email_auth():
+        logger = logging.getLogger()
+        logger.setLevel(logging.INFO)
+        logger.addHandler(handler)
         req = request.get_json()
         input = req["input"]
-        print(input)
-        print(type(input))
+        logger.info(jsonify(req))
         nonce = random.randint(
             0,
             sys.maxsize,
         )
-        proof = gen_email_auth_proof(str(nonce), False, input)
+        logger.info(nonce)
+        proof = gen_email_auth_proof(str(nonce), False, input, logger)
+        logger.info(jsonify(proof))
         return jsonify(proof)
 
     return app
