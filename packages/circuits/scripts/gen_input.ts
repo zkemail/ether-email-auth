703ce4edd248b2ab77c3a5b1027f99f74c5037a8
SoraSuegami <suegamisora@gmail.com>
2024-09-06 22:47:51 +0900

    Update emails for integration tests

--- packages/circuits/scripts/gen_input.ts
+++ packages/circuits/scripts/gen_input.ts
@@ -70,7 +70,9 @@ async function generate() {
             maxHeaderLength: 1024,
             maxBodyLength: 1024,
             ignoreBodyHashCheck: false,
+            // shaPrecomputeSelector: '(<(=\r\n)?d(=\r\n)?i(=\r\n)?v(=\r\n)? (=\r\n)?i(=\r\n)?d(=\r\n)?=3D(=\r\n)?"(=\r\n)?[^"]*(=\r\n)?z(=\r\n)?k(=\r\n)?e(=\r\n)?m(=\r\n)?a(=\r\n)?i(=\r\n)?l(=\r\n)?[^"]*(=\r\n)?"(=\r\n)?[^>]*(=\r\n)?>(=\r\n)?)(=\r\n)?([^<>/]+)(<(=\r\n)?/(=\r\n)?d(=\r\n)?i(=\r\n)?v(=\r\n)?>(=\r\n)?)'
         });
+        console.log(circuitInputs.padded_body.length);
         log("\n\nGenerated Inputs:", circuitInputs, "\n\n");
 
         await promisify(fs.writeFile)(args.inputFile, JSON.stringify(circuitInputs, null, 2));
