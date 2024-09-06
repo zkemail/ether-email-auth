6edbe7a33bd7fdfaee866f9fd17f79300ff1cea6
SoraSuegami <suegamisora@gmail.com>
2024-09-06 22:57:32 +0900

    Update yarn.lock

--- packages/circuits/scripts/gen_input.ts
+++ packages/circuits/scripts/gen_input.ts
@@ -70,7 +70,7 @@ async function generate() {
             maxHeaderLength: 1024,
             maxBodyLength: 1024,
             ignoreBodyHashCheck: false,
-            // shaPrecomputeSelector: '(<(=\r\n)?d(=\r\n)?i(=\r\n)?v(=\r\n)? (=\r\n)?i(=\r\n)?d(=\r\n)?=3D(=\r\n)?"(=\r\n)?[^"]*(=\r\n)?z(=\r\n)?k(=\r\n)?e(=\r\n)?m(=\r\n)?a(=\r\n)?i(=\r\n)?l(=\r\n)?[^"]*(=\r\n)?"(=\r\n)?[^>]*(=\r\n)?>(=\r\n)?)(=\r\n)?([^<>/]+)(<(=\r\n)?/(=\r\n)?d(=\r\n)?i(=\r\n)?v(=\r\n)?>(=\r\n)?)'
+            shaPrecomputeSelector: '(<div id=3D\"[^\"]*zkemail[^\"]*\"[^>]*>)'
         });
         console.log(circuitInputs.padded_body.length);
         log("\n\nGenerated Inputs:", circuitInputs, "\n\n");
