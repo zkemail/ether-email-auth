cf12f6d4482163dc7c20c5217336ff9e86f55c38
Dimitri <dimitridumonet@gmail.com>
2024-09-19 17:10:30 +0700

    Ignore abis for fmt

--- packages/relayer/src/abis/mod.rs
+++ packages/relayer/src/abis/mod.rs
@@ -1,6 +1,13 @@
 #![allow(clippy::all)]
+
+
+#[cfg_attr(rustfmt, rustfmt::skip)]
 pub mod ecdsa_owned_dkim_registry;
+
+#[cfg_attr(rustfmt, rustfmt::skip)]
 pub mod email_account_recovery;
+
+#[cfg_attr(rustfmt, rustfmt::skip)]
 pub mod email_auth;
 
 pub use ecdsa_owned_dkim_registry::ECDSAOwnedDKIMRegistry;
