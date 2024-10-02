77550ea91e1d90d31627bad8b12107533506182c
wshino <wshinohara@gmail.com>
2024-10-03 01:46:44 +0900

    Move tests for JwtRegistry

--- packages/contracts/test/utils/JwtRegistry/JwtRegistry_stringToArray.t.sol
+++ packages/contracts/test/utils/JwtRegistry/JwtRegistry_stringToArray.t.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.12;
 
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
-import {EmailAuth, EmailAuthMsg} from "../../src/EmailAuth.sol";
-import {RecoveryController} from "../helpers/RecoveryController.sol";
-import {StructHelper} from "../helpers/StructHelper.sol";
-import {SimpleWallet} from "../helpers/SimpleWallet.sol";
+import {EmailAuth, EmailAuthMsg} from "../../../src/EmailAuth.sol";
+import {RecoveryController} from "../../helpers/RecoveryController.sol";
+import {StructHelper} from "../../helpers/StructHelper.sol";
+import {SimpleWallet} from "../../helpers/SimpleWallet.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 contract JwtRegistryTest_stringToArray is StructHelper {
