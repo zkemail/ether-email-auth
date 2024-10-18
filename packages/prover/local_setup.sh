976f8aaf6416e885b8acff37d65986c23c8a198a
SoraSuegami <suegamisora@gmail.com>
2024-10-18 15:37:10 +0900

    Update prover

--- packages/prover/local_setup.sh
+++ packages/prover/local_setup.sh
@@ -6,8 +6,10 @@ mkdir -p build
 npm install -g snarkjs@latest
 pip install -r requirements.txt
 mkdir build && cd build
-gdown "https://drive.google.com/uc?id=1XDPFIL5YK8JzLGoTjmHLXO9zMDjSQcJH"
-unzip params.zip
+# gdown "https://drive.google.com/uc?id=1XDPFIL5YK8JzLGoTjmHLXO9zMDjSQcJH"
+curl https://storage.googleapis.com/circom-ether-email-auth/v0.1.0-preview/email_auth.zkey --output ./email_auth.zkey
+curl https://storage.googleapis.com/circom-ether-email-auth/v0.1.0-preview/email_auth.wasm --output ./email_auth.wasm
+# unzip params.zip
 # curl https://email-wallet-trusted-setup-ceremony-pse-p0tion-production.s3.eu-central-1.amazonaws.com/circuits/emailwallet-account-creation/contributions/emailwallet-account-creation_00019.zkey --output /root/params/account_creation.zkey
 # curl https://email-wallet-trusted-setup-ceremony-pse-p0tion-production.s3.eu-central-1.amazonaws.com/circuits/emailwallet-account-init/contributions/emailwallet-account-init_00007.zkey --output /root/params/account_init.zkey
 # curl https://email-wallet-trusted-setup-ceremony-pse-p0tion-production.s3.eu-central-1.amazonaws.com/circuits/emailwallet-account-transport/contributions/emailwallet-account-transport_00005.zkey --output /root/params/account_transport.zkey
