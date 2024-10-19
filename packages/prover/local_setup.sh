9b3e29c0003f0ab61737ee01b803281227ec21cc
SoraSuegami <suegamisora@gmail.com>
2024-10-19 17:57:33 +0900

    Update prover

--- packages/prover/local_setup.sh
+++ packages/prover/local_setup.sh
@@ -8,7 +8,17 @@ pip install -r requirements.txt
 mkdir build && cd build
 # gdown "https://drive.google.com/uc?id=1XDPFIL5YK8JzLGoTjmHLXO9zMDjSQcJH"
 curl https://storage.googleapis.com/circom-ether-email-auth/v0.1.1-preview/email_auth.zkey --output ./email_auth.zkey
-curl https://storage.googleapis.com/circom-ether-email-auth/v0.1.1-preview/email_auth.wasm --output ./email_auth.wasm
+mkdir email_auth_cpp && cd email_auth_cpp
+curl https://storage.googleapis.com/circom-ether-email-auth/v0.1.1-preview/email_auth_cpp/Makefile --output ./Makefile
+curl https://storage.googleapis.com/circom-ether-email-auth/v0.1.1-preview/email_auth_cpp/calcwit.cpp --output ./calcwit.cpp
+curl https://storage.googleapis.com/circom-ether-email-auth/v0.1.1-preview/email_auth_cpp/calcwit.hpp --output ./calcwit.hpp
+curl https://storage.googleapis.com/circom-ether-email-auth/v0.1.1-preview/email_auth_cpp/circom.hpp --output ./circom.hpp
+curl https://storage.googleapis.com/circom-ether-email-auth/v0.1.1-preview/email_auth_cpp/email_auth.cpp --output ./email_auth.cpp
+curl https://storage.googleapis.com/circom-ether-email-auth/v0.1.1-preview/email_auth_cpp/email_auth.dat --output ./email_auth.dat
+curl https://storage.googleapis.com/circom-ether-email-auth/v0.1.1-preview/email_auth_cpp/fr.asm --output ./fr.asm
+curl https://storage.googleapis.com/circom-ether-email-auth/v0.1.1-preview/email_auth_cpp/fr.cpp --output ./fr.cpp
+curl https://storage.googleapis.com/circom-ether-email-auth/v0.1.1-preview/email_auth_cpp/fr.hpp --output ./fr.hpp
+curl https://storage.googleapis.com/circom-ether-email-auth/v0.1.1-preview/email_auth_cpp/main.cpp --output ./main.cpp
 # unzip params.zip
 # curl https://email-wallet-trusted-setup-ceremony-pse-p0tion-production.s3.eu-central-1.amazonaws.com/circuits/emailwallet-account-creation/contributions/emailwallet-account-creation_00019.zkey --output /root/params/account_creation.zkey
 # curl https://email-wallet-trusted-setup-ceremony-pse-p0tion-production.s3.eu-central-1.amazonaws.com/circuits/emailwallet-account-init/contributions/emailwallet-account-init_00007.zkey --output /root/params/account_init.zkey
