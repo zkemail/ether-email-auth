37b97105476884f5c960af8c6943d2f49c9571aa
SoraSuegami <suegamisora@gmail.com>
2024-10-30 00:15:45 +0900

    Disable logger temporary

--- packages/prover/modal_server.py
+++ packages/prover/modal_server.py
@@ -1,9 +1,10 @@
 import modal
-import logging
-from google.cloud.logging import Client
-from google.cloud.logging.handlers import CloudLoggingHandler
-from google.cloud.logging_v2.handlers import setup_logging
-from google.oauth2 import service_account
+
+# import logging
+# from google.cloud.logging import Client
+# from google.cloud.logging.handlers import CloudLoggingHandler
+# from google.cloud.logging_v2.handlers import setup_logging
+# from google.oauth2 import service_account
 
 app = modal.App("email-auth-prover-v1.5.2")
 
@@ -32,32 +33,32 @@ def flask_app():
     )
 
     app = Flask(__name__)
-    service_account_info = json.loads(os.environ["SERVICE_ACCOUNT_JSON"])
-    credentials = service_account.Credentials.from_service_account_info(
-        service_account_info
-    )
-    logging_client = Client(project="zkairdrop", credentials=credentials)
-    print(logging_client)
-    handler = CloudLoggingHandler(logging_client, name="ether-email-auth-prover")
-    print(handler)
-    setup_logging(handler)
+    # service_account_info = json.loads(os.environ["SERVICE_ACCOUNT_JSON"])
+    # credentials = service_account.Credentials.from_service_account_info(
+    #     service_account_info
+    # )
+    # logging_client = Client(project="zkairdrop", credentials=credentials)
+    # print(logging_client)
+    # handler = CloudLoggingHandler(logging_client, name="ether-email-auth-prover")
+    # print(handler)
+    # setup_logging(handler)
 
     @app.post("/prove/email_auth")
     def prove_email_auth():
         print("prove_email_auth")
         req = request.get_json()
         input = req["input"]
-        logger = logging.getLogger(__name__)
-        logger.info(req)
+        # logger = logging.getLogger(__name__)
+        # logger.info(req)
         print(req)
         nonce = random.randint(
             0,
             sys.maxsize,
         )
-        logger.info(nonce)
+        # logger.info(nonce)
         print(nonce)
         proof = gen_email_auth_proof(str(nonce), False, input)
-        logger.info(proof)
+        # logger.info(proof)
         print(proof)
         return jsonify(proof)
 
