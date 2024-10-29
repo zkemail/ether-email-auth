2caf0e1c7684b11036ab84f05cda6c5ec00776bf
SoraSuegami <suegamisora@gmail.com>
2024-10-29 16:48:36 +0900

    update prover

--- packages/prover/local_setup.sh
+++ packages/prover/local_setup.sh
@@ -8,7 +8,8 @@ pip install -r requirements.txt
 mkdir build && cd build
 # gdown "https://drive.google.com/uc?id=1XDPFIL5YK8JzLGoTjmHLXO9zMDjSQcJH"
 curl https://storage.googleapis.com/circom-ether-email-auth/v2.0.1-preview/email_auth.zkey --output ./email_auth.zkey
-curl https://storage.googleapis.com/circom-ether-email-auth/v2.0.1-preview/email_auth.wasm --output ./email_auth.wasm
+curl https://storage.googleapis.com/circom-ether-email-auth/v2.0.1-preview/email_auth --output ./email_auth
+
 # unzip params.zip
 # curl https://email-wallet-trusted-setup-ceremony-pse-p0tion-production.s3.eu-central-1.amazonaws.com/circuits/emailwallet-account-creation/contributions/emailwallet-account-creation_00019.zkey --output /root/params/account_creation.zkey
 # curl https://email-wallet-trusted-setup-ceremony-pse-p0tion-production.s3.eu-central-1.amazonaws.com/circuits/emailwallet-account-init/contributions/emailwallet-account-init_00007.zkey --output /root/params/account_init.zkey
