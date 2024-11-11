c97004116b8ae8fac6e99d5a1f819784ed49f6dd
SoraSuegami <suegamisora@gmail.com>
2024-11-11 16:47:23 +0900

    update zkey and verifier

--- packages/prover/modal_server.py
+++ packages/prover/modal_server.py
@@ -6,7 +6,7 @@ from google.cloud.logging.handlers import CloudLoggingHandler
 from google.cloud.logging_v2.handlers import setup_logging
 from google.oauth2 import service_account
 
-app = modal.App("email-auth-prover-v1.5.3")
+app = modal.App("email-auth-prover-v1.5.4")
 
 image = modal.Image.from_dockerfile("Dockerfile")
 
